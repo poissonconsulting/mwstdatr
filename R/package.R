2652b7328c766cdfcd6b2b25b7cf6f44d7b2dbe2
Joe Thorley <joe@poissonconsulting.ca>
2015-10-12 15:55:50 -0700

    added namespace

---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' Mountain Whitefish Spawn Timing Data
+#'
+#' \code{mwstdatr} is the R data package for the Irvine et al. manuscript
+#' When Do Mountain Whitefish (Prosopium williamsoni) Spawn?
+#' A Comparison of Estimates Based on Gonadosomatic Indices and Spawner and Egg Counts.
+#'
+#' For a description of the available data sets type \code{data(package = "mwst")}.
+#'
+#' @docType package
+#' @name mwstdatr
+NULL
